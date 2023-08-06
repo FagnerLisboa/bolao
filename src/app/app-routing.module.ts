920aa462836ff4b98a81b0ad8c4a74643b7a1e8e
FagnerLisboa <fnlisboa2016@gmail.com>
2023-08-06 14:08:59 -0300

    Add tela de visualização

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VisualizationComponent } from './visualization/visualization.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: VisualizationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
